718611bf1968d1ad3f408078e9a829472c189caf
dayframs <dayframs09@yahoo.com>
2023-09-15 22:07:31 +0100

    players set up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let player1, player2, box, c, d;
-let checkClick = false;
 let startG = false;
+let click = true;
 function startGame(){
     c = inp1.value;
     d = inp2.value;
@@ -12,24 +12,19 @@ function startGame(){
         console.log('kindly enter your name');
     }  
 }
+
 function display(a) {
-    checkClick = true;
     player1 = 'X';
     player2 = 'O';
-    let currentPlayer = player1;
     
     if (startG) {
-    // //     if (checkClick) {
-    // //         a.target.innerText = player1;
-    // //         checkClick = false;
-    // //     } else {
-    // //         a.target.innerText = player2;
-    // //         checkClick = true;
-    // //     }
-    //     // }
-
-            a.target.innerText = currentPlayer;
-            currentPlayer = currentPlayer === player1 ? player2 : player1;
+    if (click) {
+            a.target.innerText = player1;
+            click = false;
+    }else{
+        a.target.innerText = player2;
+        click = true;
+     }
      }
     else {
         alert('enter your name, please.')
